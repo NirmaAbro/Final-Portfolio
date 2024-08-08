57238cac7f9ccae03102473d324f0456f0de0ce1
Nirma Abro <NirmaAbro143@gmail.com>
2024-08-08 13:47:30 -0600

    gsap addedd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,54 @@ import React from "react";
 import image from "../images/img2.png";
 import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap/gsap-core";
 
 function Hero() {
+  useGSAP(()=>{
+    gsap.from(".pic", {
+      duration: 1.5,
+      opacity: 0,
+      y: 100,
+      rotate: 360,
+      ease: "power4.out",
+    });
+
+    gsap.from(".name", {
+      duration: 1.5,
+      opacity: 0,
+      y: 100,
+      // rotate: 360,
+      ease: "power4.out",
+    });
+
+    gsap.from(".mern", {
+      duration: 1.5,
+      opacity: 0,
+      y: 100,
+      delay: 1,
+      // rotate: 360,
+      ease: "power4.out",
+    });
+
+    gsap.from(".button", {
+      duration: 1.5,
+      opacity: 0,
+      x: -100,
+      rotate: -360,
+      ease: "power4.out",
+    });
+    
+    
+
+  });
+
   return (
     <section
       id="home"
       className="min-h-screen md:gap-7 bg-gray-800 flex flex-col md:flex-row py-10 justify-center items-center relative"
     >
-      <div className="flex-1 flex justify-center items-center">
+      <div className="flex-1 flex justify-center items-center pic">
         <img
           src={image}
           alt="Hero Pic"
@@ -19,14 +59,14 @@ function Hero() {
       <div className="flex-1 z-20 ">
         <div className="flex flex-col items-center md:items-start">
           <div className="text-center md:text-left">
-            <h1 className="text-white font-bold text-2xl md:leading-normal leading-10 md:text-3xl lg:text-5xl">
+            <h1 className="text-white font-bold text-2xl md:leading-normal name leading-10 md:text-3xl lg:text-5xl">
               Hello! <br />
               <span>
                 My Name is{" "}
                 <span className="hover:text-white text-cyan-600">Nirma Abro</span>{" "}
               </span>
             </h1>
-            <h1 className="text-lg md:text-2xl lg:text-3xl lg:mt-2 leading-5 md:leading-normal font-bold text-gray-600">
+            <h1 className="text-lg md:text-2xl mern lg:text-3xl lg:mt-2 leading-5 md:leading-normal font-bold text-gray-600">
               MERN STACK DEVELOPER
             </h1>
           </div>
@@ -34,7 +74,7 @@ function Hero() {
             <a
               href="/Nirma-Abro-Resume.pdf" // Ensure this matches the name and location of your resume file in the public folder
               download="Nirma_Abro_Resume.pdf" // Optional: Specify the name of the downloaded file
-              className="btn-primary mt-8 hover:bg-cyan-600 border-l-8 border-b-4  border-cyan-600 rounded-full p-2 hover:text-black md:text-2xl font-bold "
+              className="btn-primary button mt-8 hover:bg-cyan-600 border-l-8 border-b-4  border-cyan-600 rounded-full p-2 hover:text-black md:text-2xl font-bold "
             >
               Download Resume
             </a>
